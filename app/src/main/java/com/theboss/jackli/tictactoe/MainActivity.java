0cc4137718c2a3099468dccb65b228028060fda6
88JumpingJacks <li.jack2006@hotmail.com>
2016-02-11 21:44:26 -0500

    Added "play again" window once either player wins

--- app/src/main/java/com/theboss/jackli/tictactoe/MainActivity.java
+++ app/src/main/java/com/theboss/jackli/tictactoe/MainActivity.java
@@ -11,8 +11,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -64,6 +67,8 @@ public class MainActivity extends AppCompatActivity
             counter.animate().translationYBy(1000f).rotation(360).setDuration
                     (300);
 
+            TextView txtVw_Winner;
+
             // Check if the player won
             for (int[] winningPosition : winningPositions)
             {
@@ -73,26 +78,28 @@ public class MainActivity extends AppCompatActivity
                                 gameState[winningPosition[2]] &&
                         gameState[winningPosition[0]] != 2)
                 {
+                    txtVw_Winner = (TextView) findViewById(R.id.winner);
+
                     // Display the opposite activePlayer as the winner as we
                     // changed the activePlayer already in the if-else
                     // statements above
                     if (activePlayer == 0)
                     {
-                        Toast.makeText(getApplicationContext(), "Red " +
-                                "player wins!", Toast.LENGTH_LONG).show();
                         playerScore.put(1, playerScore.get(1) + 1);
                         Log.i("PlayerScore: ", "" + playerScore.entrySet());
                         red_score.setText(getString(R.string.red_score,
                                 playerScore.get(1)));
+
+                        txtVw_Winner.setText(R.string.red_win);
                     }
                     else
                     {
-                        Toast.makeText(getApplicationContext(), "Yellow " +
-                                "player wins!", Toast.LENGTH_LONG).show();
                         playerScore.put(0, playerScore.get(0) + 1);
                         Log.i("PlayerScore: ", "" + playerScore.entrySet());
                         yellow_score.setText(getString(R.string.yellow_score,
                                 playerScore.get(0)));
+
+                        txtVw_Winner.setText(R.string.yellow_win);
                     }
 
                     board = (GridLayout) findViewById(R.id
@@ -101,6 +108,11 @@ public class MainActivity extends AppCompatActivity
                     // Set all child elements on the game board to be
                     // unclickable after the current game has ended
                     setClickableRecursive(board, false);
+
+                    // Make the play again Linear Layout visible
+                    LinearLayout play_again = (LinearLayout) findViewById(R
+                            .id.play_again);
+                    play_again.setVisibility(View.VISIBLE);
                 }
             }
 
@@ -129,6 +141,42 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    /**
+     * Reset the board to begin a new game
+     *
+     * @param aInView
+     */
+    public void playAgain(View aInView)
+    {
+        View lView;
+
+        // Make all ImageViews invisible (remove src's)
+        for (int i = 0; i < board.getChildCount(); i++)
+        {
+            lView = board.getChildAt(i);
+
+            if (lView instanceof ImageView)
+            {
+                // Clear the image resource
+                ((ImageView) lView).setImageResource(0);
+            }
+        }
+
+        // Reset all gameState elements to 2 (no chip occupies slot)
+        for (int i = 0; i < gameState.length; i++)
+        {
+            gameState[i] = 2;
+        }
+
+        // Reset ImageViews to be clickable
+        setClickableRecursive(board, true);
+
+        // Hide the play again Linear Layout because we are starting a new game
+        LinearLayout play_again = (LinearLayout) findViewById(R.id.play_again);
+        play_again.setVisibility(View.INVISIBLE);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -147,41 +195,6 @@ public class MainActivity extends AppCompatActivity
         // Initialize player scores to 0
         playerScore.put(0, 0);
         playerScore.put(1, 0);
-
-        final Button lBtn_new_game = (Button) findViewById(R.id.btn_new_game);
-        lBtn_new_game.setOnClickListener(new View.OnClickListener()
-        {
-            /**
-             * Reset the board to begin a new game
-             *
-             * @param aInView
-             */
-            public void onClick(View aInView)
-            {
-                View lView;
-
-                // Make all ImageViews invisible (remove src's)
-                for (int i = 0; i < board.getChildCount(); i++)
-                {
-                    lView = board.getChildAt(i);
-
-                    if (lView instanceof ImageView)
-                    {
-                        // Clear the image resource
-                        ((ImageView) lView).setImageResource(0);
-                    }
-                }
-
-                // Reset all gameState elements to 2 (no chip occupies slot)
-                for (int i = 0; i<gameState.length; i++)
-                {
-                    gameState[i] = 2;
-                }
-
-                // Reset ImageViews to be clickable
-                setClickableRecursive(board, true);
-            }
-        });
     }
 
     @Override
